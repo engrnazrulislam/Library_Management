c72f9805d8c2791cb107a6f84ea3c09b1e7101ae
Mr. Md. Nazrul Islam <57587377+engrnazrulislam@users.noreply.github.com>
2025-08-20 19:40:37 +0600

    add user serializer

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from django.conf import settings
+from .serializers import UserSerializer
+User = settings.AUTH_USER_MODEL
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
