05c35a45b9db437fe6dad8c95c5a01601b8aabb6
Mr. Md. Nazrul Islam <57587377+engrnazrulislam@users.noreply.github.com>
2025-08-20 18:32:02 +0600

    setting api router

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from books.views import AuthorViewSet, BookViewSet, StatusViewSet
+from members.views import MemberViewSet
+from borrow.views import BorrowRecordViewSet
+router = DefaultRouter()
+router.register('authors', AuthorViewSet, basename='author')
+router.register('books', BookViewSet, basename='book')
+router.register('members', MemberViewSet, basename='member')
+router.register('borrow-records', BorrowRecordViewSet, basename='borrow-record')
+router.register(r'status', StatusViewSet, basename='status')
+urlpatterns = router.urls
