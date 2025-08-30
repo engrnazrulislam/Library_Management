a742021b7872ae7a717ec4561a4ee03c6eedb345
Mr. Md. Nazrul Islam <57587377+engrnazrulislam@users.noreply.github.com>
2025-08-31 00:34:26 +0600

    add api-root-views

--- library_management/urls.py
+++ library_management/urls.py
@@ -5,6 +5,7 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from .views import api_root_view
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -22,6 +23,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', api_root_view),
     # path('api-auth/', include('rest_framework.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
